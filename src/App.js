924a2edeac41b04c136cac023e3e2efefaebac1e
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-18 17:26:39 -0300

    requisito 3: ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Title />
       </>
     );
   }
