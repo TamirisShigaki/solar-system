aa8079ee27ac6d6d7bff222c57001a420959932d
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-18 20:52:03 -0300

    requisito 7: ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Missions />
       </>
     );
   }
---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions"> </div>
+    );
+  }
+}
+
+export default Missions;
