cd400e584f1897cfad76abec104fb40084600f65
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-18 20:55:55 -0300

    requisito 8: ok

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
